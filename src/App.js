ff8fec827d8ba53673f20fdc0e43bb9647708434
Ryangendel <gendel.ryan@gmail.com>
2022-05-02 12:01:39 -0600

    added createContext for all pages

--- src/App.js
+++ src/App.js
@@ -1,49 +1,61 @@
-import logo from './logo.svg';
-import { useState, useEffect } from "react";
-import AccountPage from "./pages/account"
-import JokePage from "./pages/joke"
-import './App.css';
+import logo from "./logo.svg";
+import { useState, useEffect, createContext } from "react";
+import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
+import AccountPage from "./pages/account";
+import JokePage from "./pages/joke";
+import "./App.css";
+
+export const AppContext = createContext();
 
 function App() {
+  const [balance, setBalance] = useState(100);
+  const [currentUser, setCurrentUser] = useState("Skadi");
+  const [joke, setJoke] = useState("");
+  const [apiData, setApiData] = useState([
+    { name: "Ruan", age: 1, breed: "bulldog" },
+    { name: "Skadi", age: 3, breed: "bull" },
+    { name: "Hunter", age: 5, breed: "dog" },
+  ]);
 
-  const [balance, setBalance] = useState(100)
-  const [currentUser, setCurrentUser] = useState("Skadi")
-  const [joke, setJoke] = useState("")
-  const [apiData, setApiData]=useState(
-    [
-      {name:"Ruan", age:1, breed:"bulldog"},
-      {name:"Skadi", age:3, breed:"bull"},
-      {name:"Hunter", age:5, breed:"dog"},
-      ]
-  )
-
-
-
-
-fetch("http://localhost:3002/bulldog", requestOptions)
-  .then(response => response.text())
-  .then(result => console.log(result))
-  .catch(error => console.log('error', error))
-
-  useEffect(()=>{
-    fetch("http://localhost:3002/bulldog", {
-      "method": "GET",
-      "mode": 'Access-Control-Allow-Origin'
+  useEffect(() => {
+    fetch("https://api.chucknorris.io/jokes/random", {
+      method: "GET",
+      // "mode": 'Access-Control-Allow-Origin'
     })
-    .then(unSanitizedJoke=> {
-      return unSanitizedJoke.json()
-    }).then(sanitizedJoke=>{
-      let newJoke = sanitizedJoke.value
-      setJoke(newJoke)
-    })
-  }, [])
+      .then((unSanitizedJoke) => {
+        return unSanitizedJoke.json();
+      })
+      .then((sanitizedJoke) => {
+        let newJoke = sanitizedJoke.value;
+        console.log("==================");
+        console.log(newJoke);
+        console.log("==================");
+        setJoke(newJoke);
+      });
+  }, []);
 
   return (
-    <div className="App">
-      {/* <AccountPage apiData={apiData} user={currentUser} setUser={setCurrentUser} /> */}
-      <AccountPage joke={joke} apiData={apiData} user={currentUser} setCurrentUser={setCurrentUser} />
-      <JokePage user={currentUser} />
-    </div>
+    <AppContext.Provider value={{ joke: joke, name: "Ryan", setJoke: setJoke }}>
+      <BrowserRouter>
+        <Routes>
+        <Route path="*" element={
+            <JokePage user={currentUser} />
+          }/>
+          <Route path="/account/" element={
+          <AccountPage
+            joke={joke}
+            apiData={apiData}
+            user={currentUser}
+            setCurrentUser={setCurrentUser}
+        />
+          } />
+          <Route exact path="/joke/:jokeid" element={
+            <JokePage user={currentUser} />
+          }/>
+          
+        </Routes>
+      </BrowserRouter>
+    </AppContext.Provider>
   );
 }
 
--- src/pages/joke.js
+++ src/pages/joke.js
@@ -1,9 +1,19 @@
-import { useState, useEffect } from "react";
+import React, { useState, useEffect, useContext } from "react";
+import { useParams }from "react-router-dom"
+import {AppContext} from "../App"
 
 function Joke(props) {
+  const { jokeid } = useParams();
+  const ctx = useContext(AppContext)
+  console.log("##########---CTX IS BELOW")
+  console.log(jokeid)
+  console.log(ctx)
+  console.log("##########---CTX IS ABOVE")
+
   return (
     <div className="App">
-        welcome to your bank account valued customer: {props.user}
+        welcome to your bank account valued customer: {props.user} 
+        and the joke of the day is : {ctx.joke} 
     </div>
   );
 }
